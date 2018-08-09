c3258c2d0a55ba428b0fbba5849f0c0857d74b76
dikshit.ram02@gmail.com <dikshit.ram02@gmail.com>
2018-08-08 20:34:26 -0500

    SCRK-1: Fix hot reloading.

---
+++ SaiCoconuts-RecordKeeping-React-Redux/src/components/dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Dashboard extends Component{
+    render(){
+        return(
+            <div>Hello, App!</div>
+        );
+    }
+}
+export default Dashboard;
