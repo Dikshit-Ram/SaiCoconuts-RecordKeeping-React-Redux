e76dbf962dd41c7d243fdf6ac1e0e08e447b2973
Dikshith Ram Adabala <dikshit.ram02@gmail.com>
2020-09-12 23:45:16 -0700

    this.state is now resolved.

--- src/components/cards/TransactionCard.js
+++ src/components/cards/TransactionCard.js
@@ -5,6 +5,11 @@ import '../../styles/TransactionCardStyles.css';
 import TransactionImage from '../../images/TransactionCard.jpg';
 
 class TransactionCard extends Component{
+
+    state = {
+        displayForm: true
+    }
+
     onClickDisplayForm() {
         this.props.dispatch(transactionActions.displayTransactionForm(this.state.displayTransactionForm));
     }
@@ -15,7 +20,7 @@ class TransactionCard extends Component{
                 <div id="TransactionCard" className="card" >
                     <img className="card-img-top" src={TransactionImage} alt="Transaction Image" />
                     <div className="card-body">
-                        <a href="#" className="btn btn-primary" onClick={this.onClickDisplayForm}>Quick Sell</a>
+                        <a href="#" className="btn btn-primary" onClick={this.onClickDisplayForm.bind(this)}>Quick Sell</a>
                     </div>
                 </div>
             </div>
