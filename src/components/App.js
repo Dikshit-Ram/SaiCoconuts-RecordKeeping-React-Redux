3c7df4ef83cd5aee65ee15494ad2ef6a9349da64
Dikshit Ram <dikshit.ram02@gmail.com>
2020-09-28 21:16:25 -0700

    Add stage background + Fix Transaction form (#11)

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Header from "./common/Header";
 import Dashboard from "./dashboard/Dashboard";
 import Stage from "./PlayArea/Stage";
 import "../styles/AppStyles.css";
-import CoconutImage from "../images/CoconutTree.jpg";
+import CoconutImage from "../images/Coconut.jpg";
 
 class App extends Component {
   render() {
@@ -13,15 +13,18 @@ class App extends Component {
         <div
           id="app"
           className="container-fluid"
-          style={{ backgroundImage: "url(" + CoconutImage + ")" }}
+          style={{
+            backgroundImage: "url(" + CoconutImage + ")",
+            backgroundSize: "contain",
+          }}
         >
           <div className="row">
-            <div className="col-sm">
               <Dashboard />
-            </div>
           </div>
-          <div className="row">
-            <Stage />
+          <div className="row d-flex justify-content-center">
+            <div className="col-md-6 bg-dark text-white">
+              <Stage />
+            </div>
           </div>
         </div>
       </div>
